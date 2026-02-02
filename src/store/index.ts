e62a8d40c8f7499405b05278d2e58d3706732556
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-02-02 14:58:13 +0100

    feat: configure Redux store with typed hooks and middleware

---
+++ src/store/index.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import authReducer from './slices/authSlice';
+import colisReducer from './slices/colisSlice';
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        colis: colisReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // Ignore these action types
+                ignoredActions: ['auth/login/fulfilled'],
+            },
+        }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Typed hooks
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
